e72ad968e1e31a307d753ce1b080ec7895cc788d
Will Braynen <wbraynen@gmail.com>
2022-01-01 02:36:27 -0700

    Added a GET call, beginnings of a Weather service

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello world!");
+#[tokio::main]
+async fn main() {
+    let response = reqwest::get("https://api.openweathermap.org/data/2.5/weather?q=corvallis&appid=b98e3f089c86867862f28236d174368a&&units=imperial")
+        .await
+        // each response is wrapped in a `Result` type
+        // we'll unwrap here for simplicity
+        .unwrap()
+        .text()
+        .await;
+    println!("{:?}", response);
 }
